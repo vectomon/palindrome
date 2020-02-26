d2a37065378538f0926b3d3ef198b75320276ddb
hxi <hidi.tamas@danubiusinfo.hu>
2020-02-26 16:32:26 +0100

    Initial Solution

---
+++ Palindrome.Logic/PalindromeExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Palindrome.Logic
+{
+    public static class PalindromeExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            for (var i = 0; i < input.Length / 2; i++)
+            {
+                if (input[i] != input[^(i + 1)])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool IsPalindrome2(this string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            var trimmedLower = value.Replace(" ", string.Empty).Trim().ToLower();
+            return trimmedLower.Reverse().SequenceEqual(trimmedLower);
+        }
+    }
+}
